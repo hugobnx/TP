beeb50c8b6d80013c153e3271ba176d29953b6a5
hugobnx <122907391+hugobnx@users.noreply.github.com>
2023-02-01 18:53:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,4 +29,31 @@ if (mysqli_query($conn, $sql)) {
 
 // Fermer la connexion
 mysqli_close($conn);
+
+// Récupération des données du formulaire de connexion
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Connexion à la base de données
+$db = mysqli_connect('localhost', 'dbuser', 'dbpassword', 'database_name');
+
+// Requête pour récupérer les informations de l'utilisateur depuis la base de données
+$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = mysqli_query($db, $query);
+
+// Vérification des informations de l'utilisateur
+if (mysqli_num_rows($result) == 1) {
+  // Les informations sont valides, démarrage de la session
+  session_start();
+  $_SESSION['username'] = $username;
+  header('Location: tableau.php');
+} else {
+  // Les informations sont incorrectes, retour à la page de connexion
+  header('Location: login.php?error=invalid');
+}
+
+// Fermeture de la connexion à la base de données
+mysqli_close($db);
+
 ?>
+
